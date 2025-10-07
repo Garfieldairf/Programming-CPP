8f871b1bca0a0f6493679bdbf97cd88f6158faea
Garfieldairf <684244127@mail.pbru.ac.th>
2025-10-07 12:18:35 +0700

    Update Chapter32-2.cpp

--- Chapter32-36/Chapter32-2.cpp
+++ Chapter32-36/Chapter32-2.cpp
@@ -1,20 +1,6 @@
-mylibrary.h:
-void myfunction();  //function declaration
-mylibrary.cpp:
 #include "mylibrary.h"
-#include <iostream>
-// function definition
-void myfunction()
-{
-    std::cout << "Hello World from multiple files.";
-}
-source.cpp:
-#include "mylibrary.h"
+
 int main()
 {
     myfunction();
 }
-#ifndef MY_LIBRARY_H
-#define MY_LIBRARY_H
-void myfunction();
-#endif // !MY_LIBRARY_H
